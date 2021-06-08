1b9a7e601b034a272c2f990ca2f123e04bc08c65
HyeonJeong <yoonhj97@naver.com>
2021-06-08 18:24:58 +0900

    [MOD] scrollY 변수 경고부분 수정

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const PinkBox = React.lazy(() => import("./boxes/PinkBox"))
 const BlueBox = React.lazy(() => import("./boxes/BlueBox"))
 const YellowBox = React.lazy(() => import("./boxes/YellowBox"))
 
-const boxArray = [<PinkBox key={1}/>, <BlueBox key={2}/>, <YellowBox key={3}/>]
+const boxArray = [<PinkBox key={1}/>, <BlueBox key={2}/>, <YellowBox key={3}/>, <PinkBox key={4}/>]
 const height = window.innerHeight - 45; // 헤더높이 빼기
 const scrollSpeed = 200; //몇초동안 이동할것인지
 let exScroll = 0; // 이전 scrollTop 위치 저장
@@ -14,28 +14,46 @@ let exScroll = 0; // 이전 scrollTop 위치 저장
 const scrolling = (scrollTop, size, goal, isDown) => {
   const frame = Math.ceil(scrollSpeed * (1 / 15))
   let scrollY = scrollTop;
+
+  const setScrollY = (value, state) => {
+    switch (state) {
+      case 'set':
+        scrollY = value
+        break;
+      case 'add':
+        scrollY += value
+        break;
+      default:
+        break;
+    }
+  }
+
+  const getScrollY = () => {
+    return scrollY
+  }
+
   for (let i = 0; i < frame; i++) {
     setTimeout(() => {
       if (i !== frame - 1) {
-        scrollY += isDown ? size / frame : size / frame * -1
+        setScrollY(isDown ? size / frame : size / frame * -1, 'add')
       } else {
-        scrollY = goal
+        setScrollY(goal, 'set')
       }
-      document.getElementById("scrollView").scrollTo(0, scrollY)
+      document.getElementById("scrollView").scrollTo(0, getScrollY())
     }, scrollSpeed / frame * i)
   }
 }
 
-const scrollAuto = async (e) => {
+const scrollAuto = (e) => {
   const scrollTop = e.target.scrollTop
   if (scrollTop - exScroll > 0) {
-    if (scrollTop % height > 0 && scrollTop % height < 5) {
+    if (scrollTop % height > 0 && scrollTop % height < 10) {
       const goal = (Math.floor(scrollTop / height) + 1) * height
       const size = goal - scrollTop
       scrolling(scrollTop, size, goal, true)
     }
   } else {
-    if (scrollTop % height < height && scrollTop % height > height - 5) {
+    if (scrollTop % height < height && scrollTop % height > height - 10) {
       const goal = Math.floor(scrollTop / height) * height
       const size = scrollTop - goal
       scrolling(scrollTop, size, goal, false)
